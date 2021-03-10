629c01dbc298013b7ede557693dad800690ab93b
79anvi <79andvik@gmail.com>
2021-03-10 23:24:44 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  len = str.length-1;
+  lenB = bracketsConfig.length;
+  arr = str.split('');
+
+  t = 'No changes'; 
+
+  for (i = 0; i < len; i++) {   
+    for (k = 0; k < lenB; k++) {
+      if (arr[i] === bracketsConfig[k][0] && arr[i+1] === bracketsConfig[k][1]){
+        arr.splice(i,2);
+        t = arr.join('');
+        if (t === '') break;
+        else check(t, bracketsConfig);
+      }    
+    }      
+  }  
+ return (t === '') ? true : false;
 }
+
+
